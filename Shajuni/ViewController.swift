15e51632ee4a1ed9ea80f7b8a0c36c7117848314
Soumen Ghosh <soumeng007@gmail.com>
2018-02-23 21:19:17 +0600

    Initial Commit

---
+++ Shajuni/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shajuni
+//
+//  Created by SOUMEN GHOSH on 23/2/18.
+//  Copyright © 2018 Sg106. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
